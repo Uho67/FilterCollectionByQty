9597479b0563b94ba7d2caae7cbb3b9ef18fff9b
uho0613 <uho0613@gmail.com>
2019-07-03 18:17:45 +0300

    first commit

---
+++ Observer/MyObserver.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uho0613
+ * Date: 02.07.19
+ * Time: 22:48
+ */
+
+namespace SysPerson\Practic\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Magento\Catalog\Model\Layer\Resolver;
+class MyObserver extends AbstractCollection implements ObserverInterface
+{
+
+    protected $_registry;
+    protected $layerResolver;
+    public function __construct(Resolver $resolve){
+        $this->layerResolver = $resolve;
+
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $collection = $observer->getCollection();
+        $category = $this->layerResolver->get()->getCurrentCategory();
+        if($category->getId() == 4) {
+            $collection = $collection->addAttributeToSelect('*');
+            $collection->joinField(
+                'qty', 'cataloginventory_stock_item', 'qty', 'product_id=entity_id', '{{table}}.stock_id=1', 'left'
+            );
+            $collection->addAttributeToFilter('qty', ['gt' => 100])->load();
+        }
+
+    }
+
+
+}
